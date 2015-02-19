79398bac79e55f149cc9c8835d26a840ef993353
Kamil Pluta <kamil@skimlinks.com>
2015-02-19 17:51:28 +0000

    some map exercises done

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -48,13 +48,13 @@
     (and (contains-point? outer i_point_1) (contains-point? outer i_point_2))))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
 (defn add-author [book new-author]
   :-)
